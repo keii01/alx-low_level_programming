068025e0d78b0aad803b1d6dd75357209700d5a3
keii01 <karimm.elearning@gmail.com>
2023-10-10 18:46:04 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
-* print_alphabet - function prototype to print alphabet in lowercase,
-* followed by a new line
-*/
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
-void print_alphabet(void)
+int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
+	return (0);
 }
